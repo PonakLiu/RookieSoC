4d85dd49075defe7a5a4486a4f79f8a72bc63f14
Liu Sir <invisible@qq.com>
2022-05-16 15:54:24 +0800

    test

---
+++ bus/bus_slave_mux.v
@@ -0,0 +1,15 @@
+`include "include/stddef.h"
+`include "bus/bus.h"
+
+module bus_slave_mux (
+    input wire cs_0_,
+    input wire cs_1_,
+    input wire cs_2_,
+    input wire cs_3_,
+    input wire cs_4_,
+    input wire cs_5_,
+    input wire cs_6_,
+    input wire cs_7_
+);
+
+endmodule //bus_slave_mux
