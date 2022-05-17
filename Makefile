66e52fb8b3e85d43f7f2c1d0102e81e0ebb43d37
Liu Sir <invisible@qq.com>
2022-05-17 15:53:08 +0800

    bus done.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ clean:
 	rm -rf sim.log
 	rm -rf *.vcd
 compile:
-	vcs -f flist -top bus_arbiter -full64 -sverilog -LDFLAGS -Wl,--no-as-needed -debug_access+r -cpp g++-4.8 -cc gcc-4.8
+	vcs -f flist -top bus -full64 -sverilog -LDFLAGS -Wl,--no-as-needed -debug_access+r -cpp g++-4.8 -cc gcc-4.8
 simulate:
 	./simv -l sim.log
 show:
--- bus/bus.v
+++ bus/bus.v
@@ -47,25 +47,25 @@ module bus (
     input wire [`DATA_WIDTH - 1:0] s6_rd_data,
     input wire [`DATA_WIDTH - 1:0] s7_rd_data,
 
-    output reg m0_grnt_,
-    output reg m1_grnt_,
-    output reg m2_grnt_,
-    output reg m3_grnt_,
-    output reg out_ready_,
-    output reg [`DATA_WIDTH - 1:0] out_rd_data,
+    output wire m0_grnt_,
+    output wire m1_grnt_,
+    output wire m2_grnt_,
+    output wire m3_grnt_,
+    output wire out_ready_,
+    output wire [`DATA_WIDTH - 1:0] out_rd_data,
 
-    output reg cs_0_,
-    output reg cs_1_,
-    output reg cs_2_,
-    output reg cs_3_,
-    output reg cs_4_,
-    output reg cs_5_,
-    output reg cs_6_,
-    output reg cs_7_,
-    output reg [`ADDR_WIDTH - 1:0] out_addr,
-    output reg out_as_,
-    output reg out_rw,
-    output reg [`DATA_WIDTH - 1:0] out_wr_data
+    output wire cs_0_,
+    output wire cs_1_,
+    output wire cs_2_,
+    output wire cs_3_,
+    output wire cs_4_,
+    output wire cs_5_,
+    output wire cs_6_,
+    output wire cs_7_,
+    output wire [`ADDR_WIDTH - 1:0] out_addr,
+    output wire out_as_,
+    output wire out_rw,
+    output wire [`DATA_WIDTH - 1:0] out_wr_data
 );
     wire [`ADDR_WIDTH - 1:0] addr;
 
@@ -80,6 +80,74 @@ module bus (
         .m1_grnt_(m1_grnt_),
         .m2_grnt_(m2_grnt_),
         .m3_grnt_(m3_grnt_)
+    );
+
+    bus_master_mux bue_master_mux_0 (
+        .m0_grnt_(m0_grnt_),
+        .m1_grnt_(m1_grnt_),
+        .m2_grnt_(m2_grnt_),
+        .m3_grnt_(m3_grnt_),
+        .m0_addr(m0_addr),
+        .m1_addr(m1_addr),
+        .m2_addr(m2_addr),
+        .m3_addr(m3_addr),
+        .m0_as_(m0_as_),
+        .m1_as_(m1_as_),
+        .m2_as_(m2_as_),
+        .m3_as_(m3_as_),
+        .m0_rw(m0_rw),
+        .m1_rw(m1_rw),
+        .m2_rw(m2_rw),
+        .m3_rw(m3_rw),
+        .m0_wr_data(m0_wr_data),
+        .m1_wr_data(m1_wr_data),
+        .m2_wr_data(m2_wr_data),
+        .m3_wr_data(m3_wr_data),
+        .out_addr(out_addr),
+        .out_as_(out_as_),
+        .out_rw(out_rw),
+        .out_wr_data(out_wr_data)
+    );
+
+    bus_addr_dec bus_addr_dec_0 (
+        .in_addr(out_addr),
+        .cs_0_(cs_0_),
+        .cs_1_(cs_1_),
+        .cs_2_(cs_2_),
+        .cs_3_(cs_3_),
+        .cs_4_(cs_4_),
+        .cs_5_(cs_5_),
+        .cs_6_(cs_6_),
+        .cs_7_(cs_7_)
+    );
+
+    bus_slave_mux bus_slave_mux_0 (
+        .cs_0_(cs_0_),
+        .cs_1_(cs_1_),
+        .cs_2_(cs_2_),
+        .cs_3_(cs_3_),
+        .cs_4_(cs_4_),
+        .cs_5_(cs_5_),
+        .cs_6_(cs_6_),
+        .cs_7_(cs_7_),
+        .s0_ready_(s0_ready_),
+        .s1_ready_(s1_ready_),
+        .s2_ready_(s2_ready_),
+        .s3_ready_(s3_ready_),
+        .s4_ready_(s4_ready_),
+        .s5_ready_(s5_ready_),
+        .s6_ready_(s6_ready_),
+        .s7_ready_(s7_ready_),
+        .s0_rd_data(s0_rd_data),
+        .s1_rd_data(s1_rd_data),
+        .s2_rd_data(s2_rd_data),
+        .s3_rd_data(s3_rd_data),
+        .s4_rd_data(s4_rd_data),
+        .s5_rd_data(s5_rd_data),
+        .s6_rd_data(s6_rd_data),
+        .s7_rd_data(s7_rd_data),
+        .out_ready_(out_ready_),
+        .out_rd_data(out_rd_data)
     );
     
 endmodule
